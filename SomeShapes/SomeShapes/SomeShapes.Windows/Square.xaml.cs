8d9709a6ec78256757c18a7e8d1cf0002779fe8e
Alex Novak <alexwnovak@gmail.com>
2014-07-04 16:14:18 -0400

    +Square empty class

---
+++ SomeShapes/SomeShapes/SomeShapes.Windows/Square.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace SomeShapes
+{
+   public sealed partial class Square : UserControl
+   {
+      public Square()
+      {
+         InitializeComponent();
+      }
+   }
+}
